0fdba126fc61d13f5f6db83065c34a1ff0dccf9c
Vanquess <libra14xu@gmail.com>
2021-01-03 12:46:16 +0800

    Start

---
+++ src/main/java/com/xsx/cash_register_system/CashRegisterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.xsx.cash_register_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashRegisterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashRegisterSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xsx/cash_register_system/CashRegisterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsx.cash_register_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashRegisterSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
